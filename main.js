94d58c72a9cfc72b37f54befc50abbe7fb686499
Anton Cao <Anton-Cao@users.noreply.github.com>
2015-04-10 22:56:21 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
-var fingers = ["Thumb", "Pointer finger", "Middle finger", "Ring finger", "Pinkie"];
+var fingers = ["thumb", "pointer finger", "middle finger", "ring finger", "pinkie"];
+var start = false;
+var currentKey=0;
 
+function randomString(){
+  var finger = Math.floor((Math.random() * 5));
+  if(finger > 4){
+    finger = 3;
+  }
+
+  var key = Math.floor((Math.random() * 26)) + 65;
+  if(key >= 91){
+    key = 90;
+  }
+
+  currentKey = key;
+  var string = "Place your " + fingers[finger] + " on " + String.fromCharCode(key);
+  return string;
+
+}
 
 $(document).ready(function(){
 
+  $(document).click(function(){
+    if(!start){
+      start=true;
+      var command = randomString();
+      $("#text").text(command);
+    }
+
+  });
+
   $(document).keydown(function(e){
+    if(start){
+
+
+
+
+    }
+  });
+
+  $(document).keyup(function(e){
+
+
 
 
   });
